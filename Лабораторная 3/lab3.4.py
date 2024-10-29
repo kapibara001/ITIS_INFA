56ca39953ab41d968f0acf4aef0f262bdc77e09e
kapibara001 <136171131+kapibara001@users.noreply.github.com>
2024-10-29 12:06:59 +0500

    full_code

---
+++ Лабораторная 3/lab3.4.py
@@ -0,0 +1,19 @@
+import random
+    
+rows = 5 # количество строк
+cols = 4 # количество столбцов
+    
+array=[[random.randint(0,100) for _ in range(cols)] for _ in range(rows)]
+    
+for row in array:
+    print(row)
+
+maxEven=0
+
+for i in range(len(array)):
+    for j in range(len(array[i])):
+        #if array[i][j]%2==0 and maxEven< array[i][j]:
+        if array[i][j]%2==0 and maxEven< array[i][j] and i == j:
+            maxEven = array[i][j]
+
+print(maxEven)
---
+++ Лабораторная 3/lab3.5.py
@@ -0,0 +1,28 @@
+def edin(quantity):
+    row = [1 for _ in range(quantity)]
+    return row
+
+def edin_zero(quantity):
+    row = [0 for _ in range(quantity-1)]
+    row.insert(0, 1)
+    return row 
+
+def zero_edin(quantity):
+    row = [0 for _ in range(quantity-1)]
+    row.insert(quantity, 1)
+    return row 
+
+import random
+
+rows = int(input("Количество строк: "))
+clmns = int(input("Количество стобцов: "))
+
+#array = [[random.randint(1,20) for _ in range(clmns)] for _ in range(rows)]
+
+for i in range(1, rows+1):
+    if i%2 != 0:
+        print(edin(clmns))
+    if i%2==0 and i%4!=0:
+        print(zero_edin(clmns))
+    if i%4==0:
+        print(edin_zero(clmns))
