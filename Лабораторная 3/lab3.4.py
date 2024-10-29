8322752bc393d6dc36952aa05898ee988bb1cd23
kapibara001 <136171131+kapibara001@users.noreply.github.com>
2024-10-29 23:03:49 +0500

    forbase

--- Лабораторная 3/lab3.4.py
+++ Лабораторная 3/lab3.4.py
@@ -1,6 +1,6 @@
 import random
     
-rows = 5 # количество строк
+rows = 5 # количество строк...
 cols = 4 # количество столбцов
     
 array=[[random.randint(0,100) for _ in range(cols)] for _ in range(rows)]
@@ -13,7 +13,7 @@ maxEven=0
 for i in range(len(array)):
     for j in range(len(array[i])):
         #if array[i][j]%2==0 and maxEven< array[i][j]:
-        if array[i][j]%2==0 and maxEven< array[i][j] and i == j:
+        if array[i][j]%2==0 and maxEven< array[i][j] and i == j: # Задание после выполнения базы
             maxEven = array[i][j]
 
 print(maxEven)
